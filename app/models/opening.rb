8c6792d16f6f8acaf93fd897a709f4ec671bba6c
Raphaël Libert <rlibert.externe@g7.fr>
2018-01-22 16:45:03 +0100

    opening  presence validation

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -1,4 +1,9 @@
 class Opening < ActiveRecord::Base
   # Association
   belongs_to :calendar
+
+  #Validations
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+ 
 end
--- test/models/opening_test.rb
+++ test/models/opening_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class OpeningTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "should verify start_date, end_date presence" do
+	  opening_zero = openings(:zero)
+	  assert_not opening_zero.valid? 
+
+	  opening_one = openings(:one)
+	  assert opening_one.valid? 
+	end
+
+	# test "should start after day begining boundary" do
+	#   opening_one = openings(:one)
+	#   assert opening_one.valid? 
+	# end
+
+	# test "should not allow openings ovelapping" do
+	#   opening_two = openings(:two)
+	#   opening_three = openings(:three)
+	#   assert_not opening_three.valid?
+	#   assert_equal 
+	# end
+
+
 end
