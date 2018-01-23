dea8e306e518712ce03f031be9e79c501df33f69
Raphaël Libert <rlibert.externe@g7.fr>
2018-01-23 13:52:06 +0100

    validates_timeliness for opening validations

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -2,8 +2,15 @@ class Opening < ActiveRecord::Base
   # Association
   belongs_to :calendar
 
+  DAYTIME_BEGINING = '8:00am'
+  DAYTIME_ENDING = '6:00pm'
+
   #Validations
   validates :start_at, presence: true
   validates :end_at, presence: true
- 
+
+  validates_time :start_at, between: [DAYTIME_BEGINING, DAYTIME_ENDING] # On or after 8:00AM and on or before 6:00PM
+  validates_time :end_at, between: [DAYTIME_BEGINING, DAYTIME_ENDING] # On or after 8:00AM and on or before 6:00PM
+  validates_datetime :end_at, after: :start_at
+
 end
--- test/models/opening_test.rb
+++ test/models/opening_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class OpeningTest < ActiveSupport::TestCase
 
-	test "should verify start_date, end_date presence" do
+	test "should verify start_at, end_at presence" do
 	  opening_zero = openings(:zero)
 	  assert_not opening_zero.valid? 
 
@@ -10,10 +10,20 @@ class OpeningTest < ActiveSupport::TestCase
 	  assert opening_one.valid? 
 	end
 
-	# test "should start after day begining boundary" do
-	#   opening_one = openings(:one)
-	#   assert opening_one.valid? 
-	# end
+	test "should verify start_at in day boundaries" do
+	  opening_out = openings(:out_begin_boundary)
+	  assert_not opening_out.valid? 
+	end
+
+	test "should verify end_at in day boundaries" do
+	  opening_out = openings(:out_end_boundary)
+	  assert_not opening_out.valid? 
+	end
+
+	test "should verify start_at after end_at" do
+	  opening_end = openings(:end_before_start)
+	  assert_not opening_end.valid? 
+	end
 
 	# test "should not allow openings ovelapping" do
 	#   opening_two = openings(:two)
